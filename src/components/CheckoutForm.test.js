e5f05d5dcc842b19917943e92e8e4bff5bb4b84b
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-01-14 11:54:38 -0500

    completed testing for checkout form

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,38 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+    render(<CheckoutForm/>);
+});
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", () => {
+    render(<CheckoutForm/>);
+
+    const firstName= screen.getByLabelText(/first name:/i);
+    const lastName= screen.getByLabelText(/last name:/i);
+    const address= screen.getByLabelText(/address:/i);
+    const city= screen.getByLabelText(/city:/i);
+    const state= screen.getByLabelText(/state:/i);
+    const zip= screen.getByLabelText(/zip:/i);
+    const submit = screen.getByRole('button');
+
+    userEvent.type(firstName, 'jess');
+    userEvent.type(lastName, 'kitch');
+    userEvent.type(address, '123 sesame street');
+    userEvent.type(city, 'orlando');
+    userEvent.type(state, 'fl');
+    userEvent.type(zip,'12345');
+    userEvent.click(submit);
+
+
+    waitFor(async () => {
+        const successMessage = await screen.findByTestId('successMessage');
+
+        expect(successMessage).toBeInTheDocument();
+    })
+});
